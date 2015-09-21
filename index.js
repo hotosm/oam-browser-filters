68263a5ff746e24d2e6307aafd7dd1d8d39a6d39
Anand Thakker <vestibule@anandthakker.net>
2015-09-21 08:47:33 -0400

    Switch to descriptive keys for combinations

--- index.js
+++ index.js
@@ -2,22 +2,18 @@ var xtend = require('xtend');
 
 var date = [
   {
-    index: 0,
     key: 'all',
     title: 'All'
   },
   {
-    index: 1,
     key: 'week',
     title: 'Last week'
   },
   {
-    index: 2,
     key: 'month',
     title: 'Last month'
   },
   {
-    index: 3,
     key: 'year',
     title: 'Last year'
   }
@@ -25,22 +21,18 @@ var date = [
 
 var resolution = [
   {
-    index: 0,
     key: 'all',
     title: 'All'
   },
   {
-    index: 4,
     key: 'low',
     title: 'Low'
   },
   {
-    index: 8,
     key: 'medium',
     title: 'Medium'
   },
   {
-    index: 12,
     key: 'high',
     title: 'High'
   }
@@ -48,12 +40,10 @@ var resolution = [
 
 var dataType = [
   {
-    index: 0,
     key: 'all',
     title: 'All Images'
   },
   {
-    index: 16,
     key: 'service',
     title: 'Image + Map Layer'
   }
@@ -64,19 +54,13 @@ var dataType = [
  * dataTypeFilter combination.
  */
 function getCombination (dateFilter, resolutionFilter, dataTypeFilter) {
-  // the indexes on each specific filter corresopnd to digits in base-4, so
-  // that for any combination of filters, they can be added to as small an
-  // integer as possible and then used as an index into the CHARS array
-  // to yield a compact key
-  var CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
   var c = {
     date: getFilter(date, dateFilter),
     resolution: getFilter(resolution, resolutionFilter),
     dataType: getFilter(dataType, dataTypeFilter),
     searchParameters: getSearchParameters(dateFilter, resolutionFilter, dataTypeFilter)
   };
-  c.key = CHARS[c.date.index + c.resolution.index + c.dataType.index];
+  c.key = [c.date.key, c.resolution.key, c.dataType.key].join('_');
 
   return c;
 }
@@ -157,3 +141,7 @@ module.exports = {
     dataType: dataType
   }
 };
+
+if (require.main === module) {
+  console.log(JSON.stringify(getAllCombinations()));
+}
